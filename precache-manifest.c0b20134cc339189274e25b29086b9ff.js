b07a232bc4695a1e18be348fee1e3e2770530e59
veenarengarajan <vrengarajan@deloitte.com>
2020-10-30 02:48:00 -0400

    Updates

---
+++ precache-manifest.c0b20134cc339189274e25b29086b9ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f580457bd8b5afea12baccf1e92b53",
+    "url": "/veenarengarajan/index.html"
+  },
+  {
+    "revision": "628a0fa03a50230a4987",
+    "url": "/veenarengarajan/static/css/2.e4c595de.chunk.css"
+  },
+  {
+    "revision": "620c0cd37e6e8d3f10e9",
+    "url": "/veenarengarajan/static/css/main.4651d7ce.chunk.css"
+  },
+  {
+    "revision": "628a0fa03a50230a4987",
+    "url": "/veenarengarajan/static/js/2.61450c6f.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/veenarengarajan/static/js/2.61450c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620c0cd37e6e8d3f10e9",
+    "url": "/veenarengarajan/static/js/main.003b300a.chunk.js"
+  },
+  {
+    "revision": "08f24e10ebea35c13fa8",
+    "url": "/veenarengarajan/static/js/runtime-main.c7b8cdd8.js"
+  },
+  {
+    "revision": "d360036ae21bbf75f942a557fb83a99c",
+    "url": "/veenarengarajan/static/media/Bg.d360036a.jpg"
+  },
+  {
+    "revision": "711682362f0a68479c58e0b3430c63f7",
+    "url": "/veenarengarajan/static/media/MinionWelcome.71168236.gif"
+  },
+  {
+    "revision": "0642f1f966b0ad670cc36ac86c6f621e",
+    "url": "/veenarengarajan/static/media/Veena.0642f1f9.png"
+  },
+  {
+    "revision": "f55b929c720cd45bf0fe46e60753472b",
+    "url": "/veenarengarajan/static/media/VeenaRengarajan_Resume.f55b929c.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/veenarengarajan/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/veenarengarajan/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/veenarengarajan/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/veenarengarajan/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/veenarengarajan/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
